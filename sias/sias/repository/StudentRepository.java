0b5bc6c4c888a447df23155bdca4c91625cc7bed
PupHam7 <cydjian1@gmail.com>
2026-05-08 14:35:37 +0800

    Delete sias/sias/repository directory

--- sias/sias/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.sias.repository;
-
-import com.system.sias.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
